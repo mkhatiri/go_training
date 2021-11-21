aa748e767c10dfecc0b2c5699f11332e3a3a578c
mkhatiri <mohammed.khatiri@atos.net>
2021-11-21 15:16:19 +0100

    basic method

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module khatiri/goProg
 
 go 1.17
+
+require github.com/nsf/gocode v0.0.0-20190302080247-5bee97b48836 // indirect
--- main.go
+++ main.go
@@ -1,11 +1,59 @@
 package main
 
-import "fmt"
-
-func main() {
-	fmt.Println("hello med")
-}
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
 
 func Addition(a int, b int) int {
 	return a + b
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("hello med"))
+}
+
+func showPatient(w http.ResponseWriter, r *http.Request) {
+
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	i, err := fmt.Fprintf(w, "Show patient number %d", id)
+	log.Println(i, err)
+}
+
+func createPatient(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		w.Header().Add("name", "med")
+
+		w.Header().Del("name")
+		// w.WriteHeader(405)
+		// w.Write([]byte("Method Not Allowed"))
+		// this is equivalent to
+		http.Error(w, "Method Not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	w.Write([]byte("createPatient"))
+}
+
+func main() {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/patient", showPatient)
+	mux.HandleFunc("/patient/create", createPatient)
+
+	log.Println("Starting server ....")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
